a3c533fbbb4af072daeff0a86ef0cf9d6e06a987
377Nika2004 <borcovaveronika337@gmail.com>
2024-12-12 16:51:18 +0300

    методы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo world!");
+        Author mia = new Author("Ли", "Мие");
+        Author shimada = new Author("Содзи", "Симада");
+
+        Book worldInDream = new Book ( "Магазин снов мистера Талергута", mia,  2020);
+        Book worldInDream2 = new Book ( "Магазин снов мистера Талергута", mia,  2020);
+        Book detective = new Book ("Хрустальная пирамида", shimada , 2024);
+
+        System.out.println(worldInDream.equals(worldInDream2));
+        System.out.println(worldInDream.equals(detective));
+
+        System.out.println(worldInDream);
+        System.out.println(detective);
+
+        worldInDream.setPublicationYear(2022);
+        System.out.println(worldInDream);
+
+        System.out.println(worldInDream.hashCode());
     }
 }
